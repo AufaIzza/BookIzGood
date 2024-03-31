e7da26021ea19d0e4a301d098d9cf12580be3da3
AufaIzza <aufashidqiizza@gmail.com>
2024-03-31 19:45:57 +0700

    added submit by enter function

--- script.js
+++ script.js
@@ -120,3 +120,13 @@ function submitQuery() {
 }
 
 bookQueryButton.addEventListener('click', submitQuery)
+
+bookQuery.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      bookQueryButton.click();
+    }
+  });
